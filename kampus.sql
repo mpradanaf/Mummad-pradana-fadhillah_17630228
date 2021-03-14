d4bc0c5a4dc028d973e49697de47cd3ba82cb9e6
mpradanaf <pradana4299@gmail.com>
2021-03-14 15:56:43 +0800

    commit pertama dari praktikum mpradanaf 17630228

---
+++ kampus.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 21, 2021 at 06:53 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `kampus`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+DROP TABLE IF EXISTS `mahasiswa`;
+CREATE TABLE IF NOT EXISTS `mahasiswa` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nim` varchar(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `alamat` varchar(200) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `telepon` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ profile.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/fotoku.jpeg" alt="Pradana Fadhillah" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">17630228</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Muhammad Pradana Fadhillah</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Rantau, 04 Februari 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0878 15843054</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">pradanafadhillah@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/Pradana Fadhillah" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/Muhammad Pradana Fadhillah" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                            
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
